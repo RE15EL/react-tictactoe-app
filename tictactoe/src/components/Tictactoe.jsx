c201a81b6bb887da39b150805f4cd0e1bed264df
reisel <reiselvalle@gmail.com>
2023-10-01 13:07:39 -0400

    add Square component

--- tictactoe/src/components/Tictactoe.jsx
+++ tictactoe/src/components/Tictactoe.jsx
@@ -1,27 +1,31 @@
+// square component
+export const Square = ({ children, index })=>{
+    return (
+        <div className="square">
+            {children}
+        </div>
+    )
+}
 
+// turns
+const turns = {
+    X: 'X',
+    O: 'O'
+};
+
+// board
+const board = Array(9).fill(null);
 
 export const Tictactoe = ()=>{
-    // turns
-    const turns = {
-        X: 'X',
-        O: 'O'
-    };
-
-    // board
-    const board = Array(9).fill(null);
 
     return (
         <div className="board">
             <h1>Tic tac toe</h1>
             <div className="game">
                 {
-                    board.map((item, index) => {
-                        return (
-                            <div className="square" key={index}>
-                                {index}
-                            </div>
-                        )
-                    })
+                    board.map((_, index) => 
+                        <Square key={index} index={index}> {index} </Square>
+                    )
                 }
             </div>
         </div>        
