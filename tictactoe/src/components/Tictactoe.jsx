d387975ba0c09a4c926046a47f924d0bb9276740
reisel <reiselvalle@gmail.com>
2023-10-01 15:37:17 -0400

    find a winner

--- tictactoe/src/components/Tictactoe.jsx
+++ tictactoe/src/components/Tictactoe.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import confetti from 'canvas-confetti' // winner effects 
 
 useState
 
@@ -31,14 +32,50 @@ export const Tictactoe = ()=>{
     // turn state
     const [turn, setTurn] = useState(turns.X);
 
+    //winner state
+    const [winner, setWinner] = useState(null); // true = winner - false = empate 
+    const winnerCombis = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+
+    // check winner
+    const checkWinner = (boardToCheck)=>{
+        for (const combis of winnerCombis) {
+            const [ a, b ,c ] = combis;
+            if (
+                boardToCheck[a] &&
+                boardToCheck[a] === boardToCheck[b] &&
+                boardToCheck[a] === boardToCheck[c]    
+            ) {
+                confetti(true);
+                return boardToCheck[a];
+            }   
+        }
+        return null;
+    }
+
     // update board 
     const updateBoard = (index) => {
+        if (board[index] || winner) return; //stop if already selected or have a winner
+
         const newBoard = [...board];
         newBoard[index] = turn;
         setBoard(newBoard);
 
         const currentTurn = turn === turns.X ? turns.O : turns.X;
         setTurn(currentTurn);
+
+        const newWinner = checkWinner(newBoard);
+        if( newWinner){
+            setWinner(newWinner)
+        }
     }
     
     return (
