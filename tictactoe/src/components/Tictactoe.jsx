f0f37e35566f5350089833a56cace8a6af43dee1
reisel <reiselvalle@gmail.com>
2023-10-01 12:53:53 -0400

    show the board

--- tictactoe/src/components/Tictactoe.jsx
+++ tictactoe/src/components/Tictactoe.jsx
@@ -1,11 +1,29 @@
 
 
 export const Tictactoe = ()=>{
+    // turns
+    const turns = {
+        X: 'X',
+        O: 'O'
+    };
+
+    // board
+    const board = Array(9).fill(null);
 
     return (
-        <>
+        <div className="board">
             <h1>Tic tac toe</h1>
-
-        </>
+            <div className="game">
+                {
+                    board.map((item, index) => {
+                        return (
+                            <div className="square" key={index}>
+                                {index}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>        
     )
 }
